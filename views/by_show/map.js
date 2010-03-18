409904ae8f38760e1bf132aa3eb957aaec31d629
Pat Allan <pat@freelancing-gods.com>
2010-03-18 20:00:36 +1100

    Unconfirmed view, and filtering out ignored tweets for show view.

--- views/by_show/map.js
+++ views/by_show/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.text && doc.show_id) {
+  if (doc.text && doc.show_id && !doc.ignore) {
     emit(doc.show_id, doc.text);
   }
 }
---
+++ views/unconfirmed/map.js
@@ -0,0 +1,5 @@
+function(doc) {
+  if (doc.text && doc.show_id && doc.classification && !doc.ignore && !doc.confirmed) {
+    emit(doc._id, doc.text);
+  }
+}
